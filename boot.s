c960573e8ece248e1d0b9b92897d3511812453ec
dlkj <github@dlkj.co.uk>
2013-12-07 22:41:05 +0000

    untabify

--- boot.s
+++ boot.s
@@ -1,9 +1,9 @@
 # Declare multiboot header constants
-.set ALIGN,	1<<0		# align loaded modules on page boundies
-.set MEMINFO,	1<<1		# provide memory map
-.set FLAGS,	ALIGN | MEMINFO	# Multiboot flags
-.set MAGIC,	0x1BADB002	# magic header detection constant
-.set CHECKSUM,	-(MAGIC + FLAGS)
+.set ALIGN,     1<<0            # align loaded modules on page boundies
+.set MEMINFO,   1<<1            # provide memory map
+.set FLAGS,     ALIGN | MEMINFO # Multiboot flags
+.set MAGIC,     0x1BADB002      # magic header detection constant
+.set CHECKSUM,  -(MAGIC + FLAGS)
 
 # Declare the header
 .section .multiboot
@@ -23,19 +23,19 @@ stack_top:
 .global _start
 .type _start, @function
 _start:
-	#setup the stack
-	movl $stack_top, %esp
+        #setup the stack
+        movl $stack_top, %esp
 
-	#call the kernel entry point
-	#call kernel_main
+        #call the kernel entry point
+        #call kernel_main
 
-	#if the kernel returns
-	#disable interupts
-	cli
-	#halt
-	hlt
+        #if the kernel returns
+        #disable interupts
+        cli
+        #halt
+        hlt
 .Lhang:
-	jmp .Lhang
+        jmp .Lhang
 
 #set size of _start to length of the bootstrap code for debugging
 .size _start, . - _start
