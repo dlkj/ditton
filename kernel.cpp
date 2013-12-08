ec201ad69b2867395a884a7d82f55eed857e0c02
dlkj <github@dlkj.co.uk>
2013-12-08 01:22:54 +0000

    Hello, world!

--- kernel.cpp
+++ kernel.cpp
@@ -8,4 +8,18 @@
 extern "C"
 void kernel_main()
 {
+  uint16_t* terminalBuffer = (uint16_t*) 0xB8000;
+  terminalBuffer[0] = 'H' | 15 << 8;
+  terminalBuffer[1] = 'e' | 15 << 8;
+  terminalBuffer[2] = 'l' | 15 << 8;
+  terminalBuffer[3] = 'l' | 15 << 8;
+  terminalBuffer[4] = 'o' | 15 << 8;
+  terminalBuffer[5] = ',' | 15 << 8;
+  terminalBuffer[6] = ' ' | 15 << 8;
+  terminalBuffer[7] = 'w' | 15 << 8;
+  terminalBuffer[8] = 'o' | 15 << 8;
+  terminalBuffer[9] = 'r' | 15 << 8;
+  terminalBuffer[10] = 'l' | 15 << 8;
+  terminalBuffer[11] = 'd' | 15 << 8;
+  terminalBuffer[12] = '!' | 15 << 8;
 }
